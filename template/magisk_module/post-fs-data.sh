d76807575cd4958e54524d2ffcdbcc6aeebf12bc
残页 <31466456+canyie@users.noreply.github.com>
2022-03-15 20:36:03 +0800

    `unshare` is not available in twoyi

--- template/magisk_module/post-fs-data.sh
+++ template/magisk_module/post-fs-data.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
 
 cd /data/adb/modules/riru-core
-unshare -m sh -c "/system/bin/app_process -Djava.class.path=rirud.apk /system/bin --nice-name=rirud riru.Daemon 24301 /sbin $(getprop ro.dalvik.vm.native.bridge)&"
+/system/bin/app_process -Djava.class.path=rirud.apk /system/bin --nice-name=rirud riru.Daemon 24301 /sbin $(getprop ro.dalvik.vm.native.bridge)&
